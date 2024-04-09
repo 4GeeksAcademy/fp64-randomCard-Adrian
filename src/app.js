ee08bcf05ffaef05b82f80de3b8e7af0182ac6d4
Adrián Araque <106025865+adrarom@users.noreply.github.com>
2024-04-09 17:30:35 +0000

    commit msg

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
 import "./style.css";
+/*Hola Hans, si estas leyendo este comentario estoy escondido entre las sombras de un cibercafé barcelonés (estoy bien pero hay mucho adolescente).
+ El tragaperras lo haré cuando vuelva a casa ya que es un proyecto más exigente a nivel tiempo, además de que me emociona hacer un tragaperras bonito
+ (y estar lo menos posible en este cibercafé que me cobran 4 euros la hora)
+ Te debo un vaso.
+ */
 const generarCarta = () => {
   const palos = ["♠", "♥", "♦", "♣"];
   const valores = [
