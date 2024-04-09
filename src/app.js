0b872a45038c8bc39d8689cd997f4daaf68d04ef
Adrián Araque <106025865+adrarom@users.noreply.github.com>
2024-04-09 17:25:35 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const generarCarta = () => {
+  const palos = ["♠", "♥", "♦", "♣"];
+  const valores = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  const paloAleatorio = palos[Math.floor(Math.random() * palos.length)];
+  const valorAleatorio = valores[Math.floor(Math.random() * valores.length)];
+  return {
+    palo: paloAleatorio,
+    valor: valorAleatorio
+  };
 };
+
+const actualizarCarta = () => {
+  const nuevaCarta = generarCarta();
+  const cartaElemento = document.getElementById("carta");
+
+  const valores = cartaElemento.querySelectorAll(".valor");
+  valores.forEach(
+    valorElemento => (valorElemento.textContent = nuevaCarta.valor)
+  );
+
+  const palos = cartaElemento.querySelectorAll(".palo");
+  palos.forEach(paloElemento => (paloElemento.textContent = nuevaCarta.palo));
+};
+
+actualizarCarta();
